cd2cb3fc864df723138fcaf978147976c08f9efd
jlassig <las22004@byui.edu>
2023-05-15 12:35:26 -0600

    seriously, NOW it's working

--- swagger.js
+++ swagger.js
@@ -2,14 +2,14 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Contact API',
+    title: "My API",
+    description: "Contact API",
   },
-  host: 'three41final.onrender.com',
+  host: "three41final.onrender.com",
+  schemes: ["https"],
   // host: 'localhost:8080',
-  schemes: ['https'],
   // schemes: ['http'],
-};
+}
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
