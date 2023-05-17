9354f62ac0f3b35f49fac75e50c05747c997ff49
jlassig <las22004@byui.edu>
2023-05-16 21:30:55 -0600

    added authenticate to other user routes

--- __tests__/post.spec.js
+++ __tests__/post.spec.js
@@ -6,20 +6,20 @@ const request = supertest(app);
 //////-----------USER TESTS
 describe("Test Handlers", () => {
   test("responds to get /user", async () => {
-    const res = await request.get("/user");
-    expect(res.statusCode).toBe(200);
-    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-  });
+    const res = await request.get("/user")
+    expect(res.statusCode).toBe(200)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
-  test("responds to post /user", async () => {
-    const res = await request.post("/user").send({
-      firstName: "Sonic",
-      lastName: "TheHedgehog",
-      gamesPlayed: "18",
-    });
-    expect(res.statusCode).toBe(201);
-    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-  });
+  //   test("responds to post /user", async () => {
+  //     const res = await request.post("/user").send({
+  //       firstName: "Sonic",
+  //       lastName: "TheHedgehog",
+  //       gamesPlayed: "18",
+  //     });
+  //     expect(res.statusCode).toBe(201);
+  //     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+  //   });
 
   // test('responds to get /user by id', async () => {
   //     const res = await request.get('/user/645dab77d10178e68110fc48')
@@ -43,46 +43,46 @@ describe("Test Handlers", () => {
 
   /////----------GAME TESTS
 
-  // test('responds to post /game', async () => {
-  //     const res = await request.post('/game').send({
-  //         gameTitle: "Myst",
-  //         releaseYear: "1993",
-  //         language: "English",
-  //         gameLength: "7.8 hours",
-  //         rating: "pg",
-  //         specialFeatures: "falling, puzzles, the joy of choosing your own adventure",
-  //         cost: "9.98"
-  //     });
-  //     expect(res.statusCode).toBe(201)
-  //     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+  test('responds to post /game', async () => {
+      const res = await request.post('/game').send({
+          gameTitle: "Myst",
+          releaseYear: "1993",
+          language: "English",
+          gameLength: "7.8 hours",
+          rating: "pg",
+          specialFeatures: "falling, puzzles, the joy of choosing your own adventure",
+          cost: "9.98"
+      });
+      expect(res.statusCode).toBe(201)
+      expect(res.header['content-type']).toBe('application/json; charset=utf-8');
 
-  // })
+  })
 
-  // test('responds to get /game', async () => {
-  //     const res = await request.get('/game')
-  //     expect(res.statusCode).toBe(200)
-  //     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-  // })
+  test('responds to get /game', async () => {
+      const res = await request.get('/game')
+      expect(res.statusCode).toBe(200)
+      expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+  })
 
-  // test('responds to get /game by id', async () => {
-  //     const res = await request.get('/game/645ead5e2afff6aad0bb77cd')
-  //     expect(res.statusCode).toBe(200)
-  //     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+  test('responds to get /game by id', async () => {
+      const res = await request.get('/game/6462eda305a5adfc35618af2')
+      expect(res.statusCode).toBe(200)
+      expect(res.header['content-type']).toBe('application/json; charset=utf-8');
 
-  // })
+  })
 
-  // test('responds to put /game', async () => {
-  //     const res = await request.put('/game/645ead5e2afff6aad0bb77cd').send({
-  //         gameTitle: "this is a test",
-  //         releaseYear: "1",
-  //         language: "English",
-  //         gameLength: "3x longer than it should be",
-  //         rating: "pg",
-  //         specialFeatures: "testing code for CSE341",
-  //         cost: "$80 per credit"
-  //     });
-  //     expect(res.statusCode).toBe(204);
-  // })
+  test('responds to put /game', async () => {
+      const res = await request.put('/game/6462eda305a5adfc35618af2').send({
+          gameTitle: "this is a test",
+          releaseYear: "1",
+          language: "English",
+          gameLength: "3x longer than it should be",
+          rating: "pg",
+          specialFeatures: "testing code for CSE341",
+          cost: "$80 per credit"
+      });
+      expect(res.statusCode).toBe(204);
+  })
 
   // test('responds to delete /game by id', async () => {
   //     const res = await request.delete('/game/64596fc7f00d9cb76d53421b');
@@ -92,45 +92,45 @@ describe("Test Handlers", () => {
 
   //////-----------PLATFORM TESTS
 
-  //   test("responds to get /platform", async () => {
-  //     const res = await request.get("/platform")
-  //     expect(res.statusCode).toBe(200)
-  //     expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
-  //   })
+  test("responds to get /platform", async () => {
+    const res = await request.get("/platform")
+    expect(res.statusCode).toBe(200)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
-  //   test("responds to post /platform", async () => {
-  //     const res = await request.post("/platform").send({
-  //       platformName: "Nintendo NES",
-  //       companyName: "Nintendo",
-  //       price: "64.00",
-  //     })
-  //     expect(res.statusCode).toBe(201)
-  //     expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
-  //   })
+  test("responds to post /platform", async () => {
+    const res = await request.post("/platform").send({
+      platformName: "Nintendo NES",
+      companyName: "Nintendo",
+      price: "64.00",
+    })
+    expect(res.statusCode).toBe(201)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
-  //   test("responds to get /platform by id", async () => {
-  //     const res = await request.get("/platform/645ef2d6145cfd41ce698dc6")
-  //     expect(res.statusCode).toBe(200)
-  //     expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
-  //   })
+  test("responds to get /platform by id", async () => {
+    const res = await request.get("/platform/645ef2d6145cfd41ce698dc6")
+    expect(res.statusCode).toBe(200)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
-  //   test('responds to put /platform', async () => {
-  //       const res = await request.put("/platform/6462efa6cabf67efb5376399").send({
-  //         platformName: "Nintendo NES",
-  //         companyName: "Nintendo",
-  //         price: "99"
-  //       })
-  //       expect(res.statusCode).toBe(204);
-  //   })
+  test("responds to put /platform", async () => {
+    const res = await request.put("/platform/6462efa6cabf67efb5376399").send({
+      platformName: "Nintendo NES",
+      companyName: "Nintendo",
+      price: "99",
+    })
+    expect(res.statusCode).toBe(204)
+  })
 
   //////------------end of platform tests
 
   /// Mod Test
   test("responds to get /mod", async () => {
-    const res = await request.get("/mod");
-    expect(res.statusCode).toBe(200);
-    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-  });
+    const res = await request.get("/mod")
+    expect(res.statusCode).toBe(200)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
   test("responds to post /mod", async () => {
     const res = await request.post("/mod").send({
@@ -138,16 +138,16 @@ describe("Test Handlers", () => {
       game: "Stardew Valley",
       description: "Adds new locations, new characters, and new village",
       cost: "$10",
-    });
-    expect(res.statusCode).toBe(201);
-    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-  });
+    })
+    expect(res.statusCode).toBe(201)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
   test("responds to get /mod by id", async () => {
-    const res = await request.get("/mod/64627ca5f0db6165530553d0");
-    expect(res.statusCode).toBe(200);
-    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-  });
+    const res = await request.get("/mod/64627ca5f0db6165530553d0")
+    expect(res.statusCode).toBe(200)
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8")
+  })
 
   test("responds to put /mod", async () => {
     const res = await request.put("/mod/64627ca5f0db6165530553d0").send({
@@ -155,9 +155,9 @@ describe("Test Handlers", () => {
       game: "Stardew Valley",
       description: "Adds new locations, new characters, and new village",
       cost: "$10",
-    });
-    expect(res.statusCode).toBe(204);
-  });
+    })
+    expect(res.statusCode).toBe(204)
+  })
 
   //////------------end of mod test
 
--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,12 @@ const userController = require('../controllers/user');
 const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', userController.getAll);
-router.get('/:id', userController.getSingle);
+router.get('/:id', isAuthenticated, userController.getSingle);
 
-// router.post('/', userController.create);
 router.post('/', isAuthenticated, userController.create);
 
-router.put('/:id', userController.modify);
-// router.put('/:id', isAuthenticated, userController.modify);
+router.put('/:id', isAuthenticated, userController.modify);
 
-router.delete('/:id', userController.deleteOne);
-// router.delete('/:id', isAuthenticated, userController.deleteOne);
+router.delete('/:id', isAuthenticated, userController.deleteOne);
 
 module.exports = router;
